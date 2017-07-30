104974d73baaa946022e4ab4b58aeaef70b98d45
RestonLaurels <Xler58@mail.ru>
2017-07-30 18:07:36 +0300

    Add files via upload

---
+++ paintscene.h
@@ -0,0 +1,36 @@
+#ifndef PAINTSCENE_H
+#define PAINTSCENE_H
+
+#include <QGraphicsScene>
+#include <QGraphicsSceneMouseEvent>
+#include <QTimer>
+#include <QDebug>
+
+#include <QWidget>
+#include <QPushButton>
+#include <QSpinBox>
+#include <QLabel>
+
+#include <vector>//
+class paintScene : public QGraphicsScene
+{
+
+    Q_OBJECT
+
+public:
+    explicit paintScene(QObject *parent = 0);
+    ~paintScene();
+
+private:
+    QPointF     previousPoint;      // Координаты предыдущей точки
+
+private:
+    // Для рисования используем события мыши
+    void mousePressEvent(QGraphicsSceneMouseEvent * event);
+
+    void mouseMoveEvent(QGraphicsSceneMouseEvent *event);
+
+};
+
+
+#endif // PAINTSCENE_H
